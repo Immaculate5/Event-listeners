ccb39fab35f618dad6a271458f31990cc2f5b41e
Immaculate5 <146907231+Immaculate5@users.noreply.github.com>
2024-02-09 18:31:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function displaySum() {
+    //function to display the sum of two numbers
+    let a = 20; //set avariable and give it a value
+    let b =30;
+    let sum =a + b; //set a sum variable and give it a value of a + b
+
+    console.log(sum); //s
+
+
+}
